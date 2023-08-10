6e670f5109f25591881251888c36b25a2f0edd8d
cankskrk <ardacankisakurek@gmail.com>
2023-08-10 20:05:47 +0300

    feat: add new POST request

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+// Multer
+const multer = require("multer");
+const upload = multer({ dest: "./public/data/uploads" });
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    res.send("File is not found!");
+  } else {
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size,
+    });
+  }
+  next();
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
